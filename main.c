72284bc0f3e504bcfd75bd85bb0f5734e3e3230c
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-11-05 23:37:04 +0900

    실습3-1

--- main.c
+++ main.c
@@ -4,20 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i,average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum=0;
 	for(i=0; i<SIZE; i++)
-	{
-		printf("input 5 score:");
-		scanf("%d", &grade[i]);
-		sum+=grade[i];
-	}
+		grade[i]= rand() % 100;
 	
-	average=sum/SIZE;
-	printf("grade average: %d\n", average);
+	for(i=0; i<SIZE; i++)
+		score[i]=grade[i];
+
+	for(i=0; i<SIZE; i++)
+		printf("score[%d]=%d\n",i,score[i]);
 	
 	return 0;
 }
