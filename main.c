82300132c4856fc6a198a2d00c906a000561e41d
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-11-05 23:20:18 +0900

    실습2-1

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SIZE 5
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int i;
-	int grade[5];
+	int grade[SIZE];
+	printf("input 5 score \n");
 	
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
-	for (i=0;i<5;i++)
-		printf("grade[%d]=%d\n",i,grade[i]);
+	for(i=0; i<SIZE; i++)
+		scanf("%d", &grade[i]);
+		
+	for( i=0; i<SIZE; i++)
+	printf("grade[%d] = %d\n", i, grade[i]);
 	
 	return 0;
 }
